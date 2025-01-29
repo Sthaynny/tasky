e56c6068633a105173608e01b6351a6e8350e9eb
sthaynnyntt <igor.costanascimento@nttdata.com>
2025-01-29 15:02:37 -0300

    feat: configurando projeto para view code

---
+++ Tasky/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasky
+//
+//  Created by Igor Costa Nascimento on 29/01/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TaskyTests/TaskyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyTests.swift
+//  TaskyTests
+//
+//  Created by Igor Costa Nascimento on 29/01/25.
+//
+
+import Testing
+@testable import Tasky
+
+struct TaskyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

60f644e014c9e20a3f98e135ebe00f1fcfcc9f91
sthaynnyntt <igor.costanascimento@nttdata.com>
2025-01-29 21:34:05 -0300

    feat: criando repositories

---
+++ Tasky/Repositories/TaskRepository.swift
@@ -0,0 +1,35 @@
+//
+//  TaskRepository.swift
+//  Tasky
+//
+//  Created by Igor Costa Nascimento on 29/01/25.
+//
+
+import Foundation
+
+
+class TaskRepository{
+    init() {
+        
+    }
+    var tasks: [Task] = []
+    
+    func addTask(_ newtask: Task){
+        tasks.append(newtask)
+    }
+    
+    func removeTask(at index:Int) -> Void {
+        guard index>0, index<tasks.count else {
+            return
+        }
+        tasks.remove(at: index)
+    }
+    
+    func completeTask(at index: Int) -> Void {
+        
+        guard index>0, index<tasks.count else {
+            return
+        }
+        tasks[index].isCompleted.toggle()
+    }
+}
